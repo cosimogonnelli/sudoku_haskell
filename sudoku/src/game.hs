1f992d44f6746d29faf597bb7bcfde7981f92ad0
Cosimo Gonnelli <cogo2@pdx.edu>
2021-02-22 09:08:50 -0800

    Add functions to check rows, columns and Blocks completion

--- sudoku/src/game.hs
+++ sudoku/src/game.hs
@@ -16,6 +16,22 @@ type Index = (Coordinate, Coordinate)
 coordinates :: [Coordinate]
 coordinates = [C0, C1, C2, C3, C4, C5, C6, C7, C8]
 
+-- List of coordinates Blocks.
+-- These are used to check each block
+-- For example block 1 is made of two coBlock1
+coBlock1 :: [Coordinate]
+coBlock1 = [C0, C1, C2]
+
+coBlock2 :: [Coordinate]
+coBlock2 = [C3, C4, C5]
+
+coBlock3 :: [Coordinate]
+coBlock3 = [C6, C7, C8]
+
+-- List of all possible number options
+numbers :: [Player]
+numbers = [One, Two, Three, Four, Five, Six, Seven, Eight, Nine]
+
 -- To check the game we need a list of rows and columns since for each row
 -- or columns we need to have numbers from 1 to 9 in order to have a valid board
 rowIndices :: Coordinate -> [Index]
@@ -72,18 +88,28 @@ instance Show Board where
 --        4 5 6
 --        7 8 9
 test :: Index -> Cell
--- Block 1
 test (x, y) =
   case (x, y) of
-    (C0, C0) -> Empty
-    (C0, C1) -> Mark Three
-    (C0, C2) -> Empty
-    (C1, C0) -> Empty
+    -- TODO Block 1 used for testing to be removed when testing the game
+    (C0, C0) -> Mark One
+    (C0, C1) -> Mark Two
+    (C0, C2) -> Mark Three
+    (C1, C0) -> Mark Five
     (C1, C1) -> Mark Four
-    (C1, C2) -> Empty
-    (C2, C0) -> Empty
-    (C2, C1) -> Empty
+    (C1, C2) -> Mark Seven
+    (C2, C0) -> Mark Eight
+    (C2, C1) -> Mark Nine
     (C2, C2) -> Mark Six
+    -- Block 1
+    -- (C0, C0) -> Empty
+    -- (C0, C1) -> Mark Three
+    -- (C0, C2) -> Empty
+    -- (C1, C0) -> Empty
+    -- (C1, C1) -> Mark Four
+    -- (C1, C2) -> Empty
+    -- (C2, C0) -> Empty
+    -- (C2, C1) -> Empty
+    -- (C2, C2) -> Mark Six
     -- Block 2
     (C0, C3) -> Empty
     (C0, C4) -> Mark Five
@@ -165,8 +191,101 @@ test (x, y) =
     (C8, C7) -> Empty
     (C8, C8) -> Mark Five
 
-showTestingBoard :: Board
-showTestingBoard = Board test
+tBoard :: Board
+tBoard = Board test
+
+-- Check if a number is in a row
+checkRow :: Board -> Player -> Coordinate -> Bool
+checkRow b p cx = Mark p `elem` [cell b (cx, y) | y <- coordinates]
+
+-- Check if all 9 numbers are in a row
+checkRowForAll :: Board -> Coordinate -> Bool
+checkRowForAll b cx = and [checkRow b n cx | n <- numbers]
+
+-- Check if every row has all 9 numbers
+checkAllRows :: Board -> Bool
+checkAllRows b = and [checkRowForAll b cx | cx <- coordinates]
+
+-- Check if a number is in a Column
+checkColumn :: Board -> Player -> Coordinate -> Bool
+checkColumn b p cy = Mark p `elem` [cell b (x, cy) | x <- coordinates]
+
+-- Check if all 9 numbers are in a column
+checkColumnForAll :: Board -> Coordinate -> Bool
+checkColumnForAll b cy = and [checkRow b n cy | n <- numbers]
+
+-- Check if every row has all 9 numbers
+checkAllColumns :: Board -> Bool
+checkAllColumns b = and [checkRowForAll b cy | cy <- coordinates]
+
+-- Below are series of check blocks functions used to check the 9 blocks of
+-- the Sudoku board. The board representations is as below:
+-- coBlock 1 2 3
+-- 1       1 2 3
+-- 2       4 5 6
+-- 3       7 8 9
+checkBlock1 :: Board -> Player -> Bool
+checkBlock1 b p = Mark p `elem` [cell b (x, y) | x <- coBlock1, y <- coBlock1]
+
+checkBlock2 :: Board -> Player -> Bool
+checkBlock2 b p = Mark p `elem` [cell b (x, y) | x <- coBlock1, y <- coBlock2]
+
+checkBlock3 :: Board -> Player -> Bool
+checkBlock3 b p = Mark p `elem` [cell b (x, y) | x <- coBlock1, y <- coBlock3]
+
+checkBlock4 :: Board -> Player -> Bool
+checkBlock4 b p = Mark p `elem` [cell b (x, y) | x <- coBlock2, y <- coBlock1]
+
+checkBlock5 :: Board -> Player -> Bool
+checkBlock5 b p = Mark p `elem` [cell b (x, y) | x <- coBlock2, y <- coBlock2]
+
+checkBlock6 :: Board -> Player -> Bool
+checkBlock6 b p = Mark p `elem` [cell b (x, y) | x <- coBlock2, y <- coBlock3]
+
+checkBlock7 :: Board -> Player -> Bool
+checkBlock7 b p = Mark p `elem` [cell b (x, y) | x <- coBlock3, y <- coBlock1]
+
+checkBlock8 :: Board -> Player -> Bool
+checkBlock8 b p = Mark p `elem` [cell b (x, y) | x <- coBlock3, y <- coBlock2]
+
+checkBlock9 :: Board -> Player -> Bool
+checkBlock9 b p = Mark p `elem` [cell b (x, y) | x <- coBlock3, y <- coBlock3]
+
+-- Check if all 9 numbers are in block 1
+checkBlock1ForAll :: Board -> Bool
+checkBlock1ForAll b = and [checkBlock1 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 2
+checkBlock2ForAll :: Board -> Bool
+checkBlock2ForAll b = and [checkBlock2 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 3
+checkBlock3ForAll :: Board -> Bool
+checkBlock3ForAll b = and [checkBlock3 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 4
+checkBlock4ForAll :: Board -> Bool
+checkBlock4ForAll b = and [checkBlock4 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 5
+checkBlock5ForAll :: Board -> Bool
+checkBlock5ForAll b = and [checkBlock5 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 6
+checkBlock6ForAll :: Board -> Bool
+checkBlock6ForAll b = and [checkBlock6 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 7
+checkBlock7ForAll :: Board -> Bool
+checkBlock7ForAll b = and [checkBlock7 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 8
+checkBlock8ForAll :: Board -> Bool
+checkBlock8ForAll b = and [checkBlock8 b n | n <- numbers]
+
+-- Check if all 9 numbers are in block 9
+checkBlock9ForAll :: Board -> Bool
+checkBlock9ForAll b = and [checkBlock9 b n | n <- numbers]
 
 -- type Board = [[Cell]]
 
